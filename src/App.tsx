db2d64e728865668944a58e1052b9a5d2cec8e9c
AnSuHyun <ansu75140301@gmail.com>
2023-11-06 22:16:23 +0900

    css 및 사이트 배경 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './css/reset.css';
+import './css/bootstrap-grid.min.css';
+import './css/animations.css';
+import './css/perfect-scrollbar.css';
+import './css/owl.carousel.css';
+import './css/magnific-popup.css';
+import './css/main.css';
+
+const App = () => {
+    return (
+        <>
+            <div className="lm-animated-bg" style={{ backgroundImage: 'url(img/main_bg.png)' }}></div>
+        </>
+    );
 }
 
 export default App;
