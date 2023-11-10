e12f37c530c0c79467eda20649dae4f7142796c8
AnSuHyun <ansu75140301@gmail.com>
2023-11-10 14:14:08 +0900

    HeaderInfo component 추가

---
+++ src/components/HeaderInfo.tsx
@@ -0,0 +1,15 @@
+const HeaderInfo = () => {
+    return (
+        <div className="header-content">
+            <div className="header-photo">
+                <img src="img/main_photo.jpg" alt="AnSuhyun" />
+            </div>
+            <div className="header-titles">
+                <h2>An Suhyun</h2>
+                <h4>Front Engineer</h4>
+            </div>
+        </div>
+    )
+};
+
+export default HeaderInfo;
--- src/container/Header.tsx
+++ src/container/Header.tsx
@@ -1,9 +1,11 @@
+import HeaderInfo from "../components/HeaderInfo";
 import SideMenu from "../components/menu/SideMenu";
 
 const Header = () => {
     return (
         <header id="site_header" className="header mobile-menu-hide">
             <SideMenu />
+            <HeaderInfo />
         </header>
     )
 };
