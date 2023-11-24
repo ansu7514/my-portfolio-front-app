a0afa9c86d4d37c3d9f26c897efecaf726df52bd
AnSuHyun <54002821+ansu7514@users.noreply.github.com>
2023-11-24 16:06:14 +0900

    Home 유저 정보 표출

--- src/components/page/HomePage.tsx
+++ src/components/page/HomePage.tsx
@@ -2,8 +2,10 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 
 import { SideMenuStatus } from "../../types/SideMenuType";
+import { UserTableType } from "../../types/DB/UserTableType";
 
 const HomePage = () => {
+    const { name, job } = useSelector((state: RootState) => state.user.info) as UserTableType;
     const sideMenuStatus = useSelector((state: RootState) => state.sideMenu.sideMenuStatus);
 
     const sectionClassName = `animated-section start-page${sideMenuStatus !== SideMenuStatus.home ? '' : ' section-active'}`
@@ -14,13 +16,10 @@ const HomePage = () => {
                 <div className="row">
                     <div className="col-sm-12 col-md-12 col-lg-12">
                         <div className="title-block">
-                            <h2>An Suhyun</h2>
+                            <h2>{name}</h2>
                             <div className="owl-carousel text-rotation">
                                 <div className="item">
-                                    <div className="sp-subtitle">2022 - ing</div>
-                                </div>
-                                <div className="item">
-                                    <div className="sp-subtitle">Frontend-Engineer</div>
+                                    <div className="sp-subtitle">{job}</div>
                                 </div>
                             </div>
                         </div>
